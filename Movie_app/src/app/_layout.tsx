e8bbfc822f48edeb2cbe84fd944575edc4910739
bharathhalemane <bharathhalemane143@gmail.com>
2026-08-23 23:59:46 +0530

    api fetch

---
+++ Movie_app/src/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "expo-router";
+import "../../global.css"
+
+export default function RootLayout() {
+  return <Stack >
+    <Stack.Screen 
+      name="(tabs)"
+      options = {{headerShown: false}}
+    />
+    {/* <Stack.Screen 
+      name="index"
+      options = {{headerShown: false}}
+    /> */}
+    <Stack.Screen 
+      name="movies/[id]"
+      options = {{headerShown: false}}
+    />
+
+  </Stack>;
+}
